a1e3c2eda8251a621112ba3761b918a297f11729
willcab <willianeduardocg13@gmail.com>
2022-09-05 19:08:10 -0500

    add logic action method for Employee controllers
    -fix error model employee
    
    Co-authored-by: HopeSaurus <hopexime@outlook.com>
    Co-authored-by: Dcastanedaan <davinsonbmx@gmail.com>
    Co-authored-by: iMartinDav <aldntmi@gmail.com>

--- app/app/controllers/employees_controller.rb
+++ app/app/controllers/employees_controller.rb
@@ -2,12 +2,18 @@ class EmployeesController < ApplicationController
   def index
     @employees = Employee.all
   end
+
   def show
     @employee = Employee.find(params[:employee])
   end
+
+  def edit
+    @employee = Employee.find(params[:employee])
+  end
+  
   def update
-    @object = Object.find(params[:id])
-      if @object.update_attributes(params[:object])
+    @object = Employee.find(employee_params)
+      if @object.update_attributes(params[:employee])
         flash[:success] = "Object was successfully updated"
         redirect_to @object
       else
@@ -15,19 +21,35 @@ class EmployeesController < ApplicationController
         render 'edit'
       end
   end
+  def new
+    @employee = Employee.new
+  end
+  
   def create
-    @object = Object.new(params[:object])
-    if @object.save
+    @employee = Object.new(employee_params)
+    if @employee.save
       flash[:success] = "Object successfully created"
-      redirect_to @object
+      redirect_to @employee
     else
       flash[:error] = "Something went wrong"
       render 'new'
     end
   end
+
+  def destroy
+    @employee = Object.find(params[:id])
+    if @employee.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to objects_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to objects_url
+    end
+  end
+  
   
   private
   def employee_params
-    params.require(:employee).permit(:name, :address)
+    params.require(:employee).permit(:name, :email, :position, :id_employee, :private_number, :store)
   end
 end
